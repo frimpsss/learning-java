a8763d2f8275f8a1a7c764da000434b7bf8a3e45
frimpsss <afrimpong031@gmail.com>
2023-11-07 10:21:55 +0000

    learn operators

---
+++ lesson-3/src/Main.java
@@ -0,0 +1,29 @@
+// operators
+// add +, multiplication *, division /, modulos %
+public class Main  {
+    public  static  void main(String [] args){
+        int a = 3;
+        int b = 2;
+
+// assignment operators
+        // increment operator
+        // -> increment by another integer
+        a = a+ 3;
+
+        a += 4;
+
+        a++; // post increment
+        ++a;// pre increment
+
+
+        System.out.println(++a);
+
+
+
+
+// relational operators (<, >, <=, >=, !, ==, !=)
+
+        boolean c = 10 < 20;
+
+    }
+}
