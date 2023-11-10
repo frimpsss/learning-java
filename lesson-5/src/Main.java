a14940c8773b49764dc041df85b5490d42881604
frimpsss <afrimpong031@gmail.com>
2023-11-10 12:50:32 +0000

    loops done

--- lesson-5/src/Main.java
+++ lesson-5/src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
    public static void main(String[] args){
+       // types of loops
+       // while, do while, for loop
+
         System.out.println("Beginning");
+
+        int i = 40;
+        // while loop
+        while (i > 11 ){
+            System.out.println("Number " + i);
+            --i;
+        }
+int j = 0;
+        // do while loop
+       do{
+           System.out.println("Hello");
+           ++j;
+       }while (j < 10);
+
+       for(int h = 0; h < 29; h++){
+           System.out.println("Hello");
+       }
     }
+
+
+//    for(int k = 0; k < 4; k++)
+//    {
+//
+//    }
+
+
 }
